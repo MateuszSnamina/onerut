de5659e8b3401dd707a53f5d5d5923dd3eda5159
MateuszSnamina <mateusz.snamina@gmail.com>
2019-02-11 18:56:34 +0100

    function unification

--- onerut_scalar/include/onerut_scalar/function_2.hpp
+++ onerut_scalar/include/onerut_scalar/function_2.hpp
@@ -13,53 +13,59 @@ namespace onerut_scalar {
     // -------------------------------------------------------------------------
 
     struct ArgInteger {
-        using BaseType = Integer;
+        using OnerutBaseType = Integer;
+        using BuildInCppType = long;
         template<class T>
         long extract(std::shared_ptr<T> arg) const;
     };
 
     struct ArgReal {
-        using BaseType = Real;
+        using OnerutBaseType = Real;
+        using BuildInCppType = double;
         template<class T>
         double extract(std::shared_ptr<T> arg) const;
     };
 
     struct ArgComplex {
-        using BaseType = Complex;
+        using OnerutBaseType = Complex;
+        using BuildInCppType = std::complex<double>;
         template<class T>
         std::complex<double> extract(std::shared_ptr<T> arg) const;
     };
 
     template<class T>
     long ArgInteger::extract(std::shared_ptr<T> arg) const {
-        static_assert(std::is_base_of<BaseType, T>::value);
+        static_assert(std::is_base_of<OnerutBaseType, T>::value);
         return arg->value_integer();
     }
 
     template<class T>
     double ArgReal::extract(std::shared_ptr<T> arg) const {
-        static_assert(std::is_base_of<BaseType, T>::value);
+        static_assert(std::is_base_of<OnerutBaseType, T>::value);
         return arg->value_real();
     }
 
     template<class T>
     std::complex<double> ArgComplex::extract(std::shared_ptr<T> arg) const {
-        static_assert(std::is_base_of<BaseType, T>::value);
+        static_assert(std::is_base_of<OnerutBaseType, T>::value);
         return arg->value_complex();
     }
 
     // -------------------------------------------------------------------------
 
     struct ReturnInteger {
-        using BaseType = Integer;
+        using OnerutBaseType = Integer;
+        using BuildInCppType = long;
     };
 
     struct ReturnReal {
-        using BaseType = Real;
+        using OnerutBaseType = Real;
+        using BuildInCppType = double;
     };
 
     struct ReturnComplex {
-        using BaseType = Complex;
+        using OnerutBaseType = Complex;
+        using BuildInCppType = std::complex<double>;
     };
 
     // -------------------------------------------------------------------------
@@ -102,53 +108,80 @@ namespace onerut_scalar {
     // -------------- FUNCTION CLASES  -----------------------------------------
     // -------------------------------------------------------------------------
 
-    template<typename Arg1, typename Callable>
-    class UnaryComplexFunction : public Complex {
+    template<class OnerutBaseType>
+    class CommonInterface {
+        // static_assert(false);
+    };
+
+    template<>
+    class CommonInterface<Real> : public Real {
+    public:
+        double value_real() const final;
+        virtual double value() const = 0;
+    };
+
+    double CommonInterface<Real>::value_real() const {
+        return value();
+    }
+
+    template<>
+    class CommonInterface<Complex> : public Complex {
+    public:
+        std::complex<double> value_complex() const final;
+        virtual std::complex<double> value() const = 0;
+    };
+
+    std::complex<double> CommonInterface<Complex>::value_complex() const {
+        return value();
+    }
+
+    template<typename ReturnTag, typename Arg1, typename Callable>
+    class UnaryFunction : public CommonInterface<typename ReturnTag::OnerutBaseType > {
     public:
         static_assert(IsArg<Arg1>::value);
-        using U1 = typename Arg1::BaseType;
-        UnaryComplexFunction(Callable callable, std::shared_ptr<U1> arg);
-        std::complex<double> value_complex() const override;
+        using U1 = typename Arg1::OnerutBaseType;
+        UnaryFunction(Callable callable, std::shared_ptr<U1> arg);
+        typename ReturnTag::BuildInCppType value() const final;
     private:
         Callable callable;
         const std::shared_ptr<U1> arg;
     };
 
-    template<typename Arg1, typename Callable>
-    UnaryComplexFunction<Arg1, Callable>::UnaryComplexFunction(
+    template<typename ReturnTag, typename Arg1, typename Callable>
+    UnaryFunction<ReturnTag, Arg1, Callable>::UnaryFunction(
             Callable callable, std::shared_ptr<U1> arg) :
     callable(callable),
     arg(arg) {
         assert(arg);
     }
 
-    template<typename Arg1, typename Callable>
-    std::complex<double>
-    UnaryComplexFunction<Arg1, Callable>::value_complex() const {
+    template<typename ReturnTag, typename Arg1, typename Callable>
+    typename ReturnTag::BuildInCppType
+    UnaryFunction<ReturnTag, Arg1, Callable>::value() const {
         const auto& x = Arg1().extract(arg);
-        const std::complex<double> y = callable(x);
+        const auto& y = callable(x);
         return y;
     }
 
     // -------------------------------------------------------------------------
 
-    template<typename Arg1, typename Arg2, typename Callable>
-    class BinaryComplexFunction : public Complex {
+    template<typename ReturnTag, typename Arg1, typename Arg2, typename Callable>
+    class BinaryFunction : public CommonInterface<typename ReturnTag::OnerutBaseType > {
     public:
         static_assert(IsArg<Arg1>::value);
         static_assert(IsArg<Arg2>::value);
-        using U1 = typename Arg1::BaseType;
-        using U2 = typename Arg2::BaseType;
-        BinaryComplexFunction(Callable callable, std::shared_ptr<U1> first_arg, std::shared_ptr<U2> second_arg);
-        std::complex<double> value_complex() const override;
+        using U1 = typename Arg1::OnerutBaseType;
+        using U2 = typename Arg2::OnerutBaseType;
+        BinaryFunction(Callable callable, std::shared_ptr<U1> first_arg, std::shared_ptr<U2> second_arg);
+        typename ReturnTag::BuildInCppType value() const final;
     private:
         Callable callable;
         const std::shared_ptr<U1> first_arg;
         const std::shared_ptr<U2> second_arg;
     };
 
-    template<typename Arg1, typename Arg2, typename Callable>
-    BinaryComplexFunction<Arg1, Arg2, Callable>::BinaryComplexFunction(
+    template<typename ReturnTag, typename Arg1, typename Arg2, typename Callable>
+    BinaryFunction<ReturnTag, Arg1, Arg2, Callable>::BinaryFunction(
             Callable callable, std::shared_ptr<U1> first_arg, std::shared_ptr<U2> second_arg) :
     callable(callable),
     first_arg(first_arg),
@@ -157,80 +190,16 @@ namespace onerut_scalar {
         assert(second_arg);
     }
 
-    template<typename Arg1, typename Arg2, typename Callable>
-    std::complex<double>
-    BinaryComplexFunction<Arg1, Arg2, Callable>::value_complex() const {
+    template<typename ReturnTag, typename Arg1, typename Arg2, typename Callable>
+    typename ReturnTag::BuildInCppType
+    BinaryFunction<ReturnTag, Arg1, Arg2, Callable>::value() const {
         const auto& firts_x = Arg1().extract(first_arg);
         const auto& second_x = Arg2().extract(second_arg);
-        const std::complex<double> y = callable(firts_x, second_x);
+        const auto y = callable(firts_x, second_x);
         return y;
     }
 
     // -------------------------------------------------------------------------
-
-    template<typename Arg1, typename Callable>
-    class UnaryRealFunction : public Real {
-    public:
-        static_assert(IsArg<Arg1>::value);
-        using U1 = typename Arg1::BaseType;
-        UnaryRealFunction(Callable callable, std::shared_ptr<U1> arg);
-        double value_real() const override;
-    private:
-        Callable callable;
-        const std::shared_ptr<U1> arg;
-    };
-
-    template<typename Arg1, typename Callable>
-    UnaryRealFunction<Arg1, Callable>::UnaryRealFunction(
-            Callable callable, std::shared_ptr<U1> arg) :
-    callable(callable),
-    arg(arg) {
-        assert(arg);
-    }
-
-    template<typename Arg1, typename Callable>
-    double
-    UnaryRealFunction<Arg1, Callable>::value_real() const {
-        const auto& x = Arg1().extract(arg);
-        const double y = callable(x);
-        return y;
-    }
-
-    // -------------------------------------------------------------------------
-
-    template<typename Arg1, typename Arg2, typename Callable>
-    class BinaryRealFunction : public Real {
-    public:
-        static_assert(IsArg<Arg1>::value);
-        static_assert(IsArg<Arg2>::value);
-        using U1 = typename Arg1::BaseType;
-        using U2 = typename Arg2::BaseType;
-        BinaryRealFunction(Callable callable, std::shared_ptr<U1> first_arg, std::shared_ptr<U2> second_arg);
-        double value_real() const override;
-    private:
-        Callable callable;
-        const std::shared_ptr<U1> first_arg;
-        const std::shared_ptr<U2> second_arg;
-    };
-
-    template<typename Arg1, typename Arg2, typename Callable>
-    BinaryRealFunction<Arg1, Arg2, Callable>::BinaryRealFunction(
-            Callable callable, std::shared_ptr<U1> first_arg, std::shared_ptr<U2> second_arg) :
-    callable(callable),
-    first_arg(first_arg),
-    second_arg(second_arg) {
-        assert(first_arg);
-        assert(second_arg);
-    }
-
-    template<typename Arg1, typename Arg2, typename Callable>
-    double
-    BinaryRealFunction<Arg1, Arg2, Callable>::value_real() const {
-        const auto& firts_x = Arg1().extract(first_arg);
-        const auto& second_x = Arg2().extract(second_arg);
-        const double y = callable(firts_x, second_x);
-        return y;
-    }
 
     // -------------------------------------------------------------------------
     // ------------PREFERRED PUBLIC API ----------------------------------------
@@ -239,55 +208,29 @@ namespace onerut_scalar {
     template <class Return, class Callable, class... Args>
     class Function;
 
-    template <class Callable, class Arg1>
-    class Function<ReturnComplex, Callable, Arg1> : public UnaryComplexFunction<Arg1, Callable> {
+    template <class Return, class Callable, class Arg1>
+    class Function<Return, Callable, Arg1> : public UnaryFunction<Return, Arg1, Callable> {
     public:
         static_assert(IsArg<Arg1>::value);
-        using U1 = typename Arg1::BaseType;
+        using U1 = typename Arg1::OnerutBaseType;
 
         Function(Callable callable, std::shared_ptr<U1> arg) :
-        UnaryComplexFunction<Arg1, Callable>(callable, arg) {
-
-        }
-    };
-
-    template <class Callable, class Arg1>
-    class Function<ReturnReal, Callable, Arg1> : public UnaryRealFunction<Arg1, Callable> {
-    public:
-        static_assert(IsArg<Arg1>::value);
-        using U1 = typename Arg1::BaseType;
-
-        Function(Callable callable, std::shared_ptr<U1> arg) :
-        UnaryRealFunction<Arg1, Callable>(callable, arg) {
+        UnaryFunction<Return, Arg1, Callable>(callable, arg) {
 
         }
 
     };
 
-    template <class Callable, class Arg1, class Arg2>
-    class Function<ReturnComplex, Callable, Arg1, Arg2> : public BinaryComplexFunction<Arg1, Arg2, Callable> {
+    template <class Return, class Callable, class Arg1, class Arg2>
+    class Function<Return, Callable, Arg1, Arg2> : public BinaryFunction<Return, Arg1, Arg2, Callable> {
     public:
         static_assert(IsArg<Arg1>::value);
         static_assert(IsArg<Arg2>::value);
-        using U1 = typename Arg1::BaseType;
-        using U2 = typename Arg2::BaseType;
+        using U1 = typename Arg1::OnerutBaseType;
+        using U2 = typename Arg2::OnerutBaseType;
 
         Function(Callable callable, std::shared_ptr<U1> first_arg, std::shared_ptr<U2> second_arg) :
-        BinaryComplexFunction<Arg1, Arg2, Callable>(callable, first_arg, second_arg) {
-        }
-    };
-
-    template <class Callable, class Arg1, class Arg2>
-    class Function<ReturnReal, Callable, Arg1, Arg2> : public BinaryRealFunction<Arg1, Arg2, Callable> {
-    public:
-        static_assert(IsArg<Arg1>::value);
-        static_assert(IsArg<Arg2>::value);
-        using U1 = typename Arg1::BaseType;
-        using U2 = typename Arg2::BaseType;
-
-        Function(Callable callable, std::shared_ptr<U1> first_arg, std::shared_ptr<U2> second_arg) :
-        BinaryRealFunction<Arg1, Arg2, Callable>(callable, first_arg, second_arg) {
-
+        BinaryFunction<Return, Arg1, Arg2, Callable>(callable, first_arg, second_arg) {
         }
     };
 
