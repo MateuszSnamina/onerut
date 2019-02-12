8dfe99b9d43b58b478d9b3018fd3e51a8dabff8f
MateuszSnamina <mateusz.snamina@gmail.com>
2019-02-12 21:53:26 +0100

    poczatek refaktoryzacji w function_scalar_function_wrapper.hpp, krok 2

--- onerut_parser/include/onerut_parser/function_scalar_function_wrapper.hpp
+++ onerut_parser/include/onerut_parser/function_scalar_function_wrapper.hpp
@@ -11,7 +11,63 @@
 
 namespace onerut_parser {
 
-    template<typename Callable>
+    template<typename T>
+    class ArgPreparator {
+    };
+
+    template<>
+    struct ArgPreparator<onerut_scalar::ArgInteger> {
+        bool do_match(const CompileResultDeref & arg) const;
+        std::shared_ptr<onerut_scalar::Integer> extract(const CompileResultDeref & arg) const;
+    };
+
+    template<>
+    struct ArgPreparator<onerut_scalar::ArgReal> {
+        bool do_match(const CompileResultDeref & arg) const;
+        std::shared_ptr<onerut_scalar::Real> extract(const CompileResultDeref & arg) const;
+    };
+
+    template<>
+    struct ArgPreparator<onerut_scalar::ArgComplex> {
+        bool do_match(const CompileResultDeref & arg) const;
+        std::shared_ptr<onerut_scalar::Complex> extract(const CompileResultDeref & arg) const;
+    };
+
+    bool
+    ArgPreparator<onerut_scalar::ArgInteger>::do_match(const CompileResultDeref & arg) const {
+        return utility::is_integer(arg);
+    }
+
+    std::shared_ptr<onerut_scalar::Integer>
+    ArgPreparator<onerut_scalar::ArgInteger>::extract(const CompileResultDeref & arg) const {
+        return utility::to_integer(arg);
+    }
+
+    bool
+    ArgPreparator<onerut_scalar::ArgReal>::do_match(const CompileResultDeref & arg) const {
+        return utility::is_real_or_integer(arg);
+    }
+
+    std::shared_ptr<onerut_scalar::Real>
+    ArgPreparator<onerut_scalar::ArgReal>::extract(const CompileResultDeref & arg) const {
+        return utility::to_real(arg);
+    }
+
+    bool
+    ArgPreparator<onerut_scalar::ArgComplex>::do_match(const CompileResultDeref & arg) const {
+        return utility::is_real_or_integer_or_complex(arg);
+    }
+
+    std::shared_ptr<onerut_scalar::Complex>
+    ArgPreparator<onerut_scalar::ArgComplex>::extract(const CompileResultDeref & arg) const {
+        return utility::to_complex(arg);
+    }
+
+    // TODO insert into future function: static_assert(onerut_scalar::IsArg<Arg1>::value);
+
+    // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+    template<typename Callable >
     std::optional<CompileResult>
     _unary_real_function(Callable callable, CompileResult arg_compile_result) {
         const auto & arg_compile_result_deref = arg_compile_result.dereference();
@@ -25,7 +81,7 @@ namespace onerut_parser {
         return std::nullopt;
     }
 
-    template<typename Callable>
+    template<typename Callable >
     std::optional<CompileResult>
     _binary_real_function(Callable callable, CompileResult first_arg_compile_result, CompileResult second_arg_compile_result) {
         const auto & first_arg_compile_result_deref = first_arg_compile_result.dereference();
@@ -42,7 +98,7 @@ namespace onerut_parser {
         return std::nullopt;
     }
 
-    template<typename Callable>
+    template<typename Callable >
     std::optional<CompileResult>
     _unary_complex_function(Callable callable, CompileResult arg_compile_result) {
         const auto & arg_compile_result_deref = arg_compile_result.dereference();
@@ -56,7 +112,7 @@ namespace onerut_parser {
         return std::nullopt;
     }
 
-    template<typename Callable>
+    template<typename Callable >
     std::optional<CompileResult>
     _binary_complex_function(Callable callable, CompileResult first_arg_compile_result, CompileResult second_arg_compile_result) {
         const auto & first_arg_compile_result_deref = first_arg_compile_result.dereference();
@@ -87,12 +143,12 @@ namespace onerut_parser {
         Callable callable;
     };
 
-    template<typename Callable>
+    template<typename Callable >
     UnaryRealFunction<Callable>::UnaryRealFunction(Callable callable) :
     callable(callable) {
     }
 
-    template<typename Callable>
+    template<typename Callable >
     CompileResult UnaryRealFunction<Callable>::get_compile_result(CompileResult arg_compile_result) const {
         const auto & arg_compile_result_deref = arg_compile_result.dereference();
         if (!arg_compile_result_deref.is_either_value_or_type())
@@ -114,12 +170,12 @@ namespace onerut_parser {
         Callable callable;
     };
 
-    template<typename Callable>
+    template<typename Callable >
     BinaryRealFunction<Callable>::BinaryRealFunction(Callable callable) :
     callable(callable) {
     }
 
-    template<typename Callable>
+    template<typename Callable >
     CompileResult BinaryRealFunction<Callable>::get_compile_result(CompileResult first_arg_compile_result, CompileResult second_arg_compile_result) const {
         const auto & first_arg_compile_result_deref = first_arg_compile_result.dereference();
         const auto & second_arg_compile_result_deref = second_arg_compile_result.dereference();
@@ -142,12 +198,12 @@ namespace onerut_parser {
         Callable callable;
     };
 
-    template<typename Callable>
+    template<typename Callable >
     UnaryComplexFunction<Callable>::UnaryComplexFunction(Callable callable) :
     callable(callable) {
     }
 
-    template<typename Callable>
+    template<typename Callable >
     CompileResult UnaryComplexFunction<Callable>::get_compile_result(CompileResult arg_compile_result) const {
         const auto & arg_compile_result_deref = arg_compile_result.dereference();
         if (!arg_compile_result_deref.is_either_value_or_type())
@@ -169,12 +225,12 @@ namespace onerut_parser {
         Callable callable;
     };
 
-    template<typename Callable>
+    template<typename Callable >
     BinaryComplexFunction<Callable>::BinaryComplexFunction(Callable callable) :
     callable(callable) {
     }
 
-    template<typename Callable>
+    template<typename Callable >
     CompileResult BinaryComplexFunction<Callable>::get_compile_result(CompileResult first_arg_compile_result, CompileResult second_arg_compile_result) const {
         const auto & first_arg_compile_result_deref = first_arg_compile_result.dereference();
         const auto & second_arg_compile_result_deref = second_arg_compile_result.dereference();
@@ -197,13 +253,13 @@ namespace onerut_parser {
         CallableComplex callable_complex;
     };
 
-    template<typename CallableReal, typename CallableComplex>
+    template<typename CallableReal, typename CallableComplex >
     UnaryRealComplexFunction<CallableReal, CallableComplex>::UnaryRealComplexFunction(CallableReal callable_real, CallableComplex callable_complex) :
     callable_real(callable_real),
     callable_complex(callable_complex) {
     }
 
-    template<typename CallableReal, typename CallableComplex>
+    template<typename CallableReal, typename CallableComplex >
     CompileResult UnaryRealComplexFunction<CallableReal, CallableComplex>::get_compile_result(CompileResult arg_compile_result) const {
         const auto & arg_compile_result_deref = arg_compile_result.dereference();
         if (!arg_compile_result_deref.is_either_value_or_type())
@@ -227,13 +283,13 @@ namespace onerut_parser {
         CallableComplex callable_complex;
     };
 
-    template<typename CallableReal, typename CallableComplex>
+    template<typename CallableReal, typename CallableComplex >
     BinaryRealComplexFunction<CallableReal, CallableComplex>::BinaryRealComplexFunction(CallableReal callable_real, CallableComplex callable_complex) :
     callable_real(callable_real),
     callable_complex(callable_complex) {
     }
 
-    template<typename CallableReal, typename CallableComplex>
+    template<typename CallableReal, typename CallableComplex >
     CompileResult BinaryRealComplexFunction<CallableReal, CallableComplex>::get_compile_result(CompileResult first_arg_compile_result, CompileResult second_arg_compile_result) const {
         const auto & first_arg_compile_result_deref = first_arg_compile_result.dereference();
         const auto & second_arg_compile_result_deref = second_arg_compile_result.dereference();
