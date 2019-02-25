27efa71b3cc83534a5bdc9c67e2b0d9a5e2a0a89
MateuszSnamina <mateusz.snamina@gmail.com>
2019-02-25 18:21:31 +0100

    poczatki diagonalizatora

---
+++ onerut_normal_operator/src/diagonalizator.cpp
@@ -0,0 +1,8 @@
+#include<cassert>
+#include<vector> 
+#include<string> 
+
+#include<onerut_normal_operator/to_mat.hpp> 
+#include<log_utils/fancy_log_mat.hpp> 
+
+#include<onerut_normal_operator/diagonalizator.hpp>
