f8054576feb49ed7f6f1e33deb9643f5d5702c4d
MateuszSnamina <mateusz.snamina@gmail.com>
2019-05-09 23:09:49 +0200

    refactor, continuation

---
+++ onerut/include/onerut/utility_ptr_map.hpp
@@ -0,0 +1,19 @@
+#ifndef ONERUT_UTILITY_PTR_MAP_HPP
+#define ONERUT_UTILITY_PTR_MAP_HPP
+
+#include<memory>
+#include<onerut/utility_ptr_transparent_comparator.hpp>
+
+template<class T, class U>
+using weak_ptr_map = std::map<std::weak_ptr<T>, U, utility::PtrTransparentComparator<T> >;
+
+template<class T, class U>
+using weak_ptr_multimap = std::multimap<std::weak_ptr<T>, U, utility::PtrTransparentComparator<T> >;
+
+template<class T, class U>
+using shared_ptr_map = std::map<std::shared_ptr<T>, U, utility::PtrTransparentComparator<T> >;
+
+template<class T, class U>
+using shared_ptr_multimap = std::multimap<std::shared_ptr<T>, U, utility::PtrTransparentComparator<T> >;
+
+#endif 
